6094611c209c3d406d5b06ea250415e3c82480e9
zhouxh <gzhou@pivotal.io>
2019-05-14 10:41:38 -0700

    simplified runit.sh

--- runit.sh
+++ runit.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#Usage: ~ instanceType serverPort isToUseLocator
-# e.g.  ~ 1 50505 true
+#Usage: ~ instanceType isToUseLocator
+# e.g.  ~ 1 false
 
-export GEMFIRE=/Users/gzhou/git_support/gemfire/open/geode-assembly/build/install/apache-geode
-#/User/gzhou/.gradle/caches/modules-2/files-2.1/org.apache.geode
-#/User/gzhou/.m2/repository/org/apache/geode/
+export GEODEINSTALL=/Users/gzhou/git12/geode/geode-assembly/build/install/apache-geode/lib
 export PROJ=`pwd`
-export THIRDPARTY=/Users/gzhou/git3/3rdparty
 
-java -Xmx2048m -cp $PROJ/build/libs/lucene_example-0.0.1.jar:$GEMFIRE/lib/geode-common-1.1.0-incubating-SNAPSHOT.jar:$GEMFIRE/lib/geode-core-1.1.0-incubating-SNAPSHOT.jar:$GEMFIRE/lib/geode-json-1.1.0-incubating-SNAPSHOT.jar:$GEMFIRE/lib/geode-lucene-1.1.0-incubating-SNAPSHOT.jar:$GEMFIRE/lib/geode-dependencies.jar:$GEMFIRE/lib/gfsh-dependencies.jar:bin examples.Main $1 $2 $3
+java -Xmx2048m -cp $GEODEINSTALL/geode-dependencies.jar:$PROJ/build/libs/lucene_example-0.0.1.jar examples.Main $1 $2
 
 
